0a526828beed8722d435fb2f6a3ab634dd08b6f6
taut0logy <raufun.ahsan@Gmail.com>
2025-02-16 22:38:21 +0600

    Update Tailwind config and CI/CD workflow with custom theme and database connection

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,8 +30,33 @@ export default {
 
     plugins: [forms, require("daisyui")],
     daisyui: {
-        themes: ["cmyk", "dark"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+        themes: [
+            {
+                "light": {
+                    "primary": "#73c2fb",
+                    "primary-focus": "#54a0eb",
+                    "primary-content": "#1f2937",
+                    "secondary": "#f000b8",
+                    "secondary-focus": "#bd0091",
+                    "secondary-content": "#ffffff",
+                    "accent": "#37cdbe",
+                    "accent-focus": "#2aa79b",
+                    "accent-content": "#ffffff",
+                    "neutral": "#c6e4ee",
+                    "neutral-focus": "#a3d7e7",
+                    "neutral-content": "#1f2937",
+                    "base-100": "#ffffff",
+                    "base-200": "#f9fafb",
+                    "base-300": "#d1d5db",
+                    "base-content": "#1f2937",
+                    "info": "#2094f3",
+                    "success": "#009485",
+                    "warning": "#ff9900",
+                    "error": "#ff5724"
+            }
+        }, "dark"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
         darkTheme: "dark", // name of one of the included themes for dark mode
+        darkMode: ['selector', '[data-theme="dark"]'], // selector for dark mode class
         base: true, // applies background color and foreground color for root element by default
         styled: true, // include daisyUI colors and design decisions for all components
         utils: true, // adds responsive and modifier utility classes
